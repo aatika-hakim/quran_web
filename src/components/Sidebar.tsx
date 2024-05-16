70805573c74916b848afafe14c39a4835a177f1c
Aatika hakim <aatikahakim90@gmail.com>
2024-05-16 13:32:16 +0500

    sidebar links added

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,9 +1,38 @@
+
 import React from 'react';
 
 function Sidebar() {
   return (
-    <div>
-      Sidebar
+    <div className='flex border-2 border-gray-100 w-72'>
+      <div className="sidebar">
+        <ul className="sidebar-menu mt-4">
+            <li>
+                <a href="#home" className="flex items-center px-6 py-4 text-gray-700 hover:bg-gray-100">
+                    Home
+                </a>
+            </li>
+            <li>
+                <a href="#network" className="flex items-center px-6 py-4 text-gray-700 hover:bg-gray-100">
+                    <i className="fas fa-user-friends mr-3" />My Network
+                </a>
+            </li>
+            <li>
+                <a href="#jobs" className="flex items-center px-6 py-4 text-gray-700 hover:bg-gray-100">
+                    <i className="fas fa-briefcase mr-3" />Jobs
+                </a>
+            </li>
+            <li>
+                <a href="#messages" className="flex items-center px-6 py-4 text-gray-700 hover:bg-gray-100">
+                    <i className="fas fa-envelope mr-3" />Messaging
+                </a>
+            </li>
+            <li>
+                <a href="#notifications" className="flex items-center px-6 py-4 text-gray-700 hover:bg-gray-100">
+                    <i className="fas fa-bell mr-3" />Notifications
+                </a>
+            </li>
+        </ul>
+    </div>
     </div>
   );
 }
